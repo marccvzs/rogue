fcdb1d6340b41008d53c53a5c691a18b323cff97
Marc Cavazos <marcavazos19@icloud.com>
2024-04-05 10:41:52 -0500

    remove env

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { ShoppingBagIcon } from '@heroicons/react/16/solid';
+
+const menuLinks = [
+    {
+        id: 1,
+        title: 'Hats',
+        url: '/hats',
+    },
+    {
+        id: 2,
+        title: 'Shirts',
+        url: '/shirts',
+    },   
+    {
+        id: 3,
+        title: 'Charms',
+        url: '/charms',
+    },
+    {
+        id: 4,
+        title: 'About Us',
+        url: '/about-us',
+    },
+];
 
 export const NavBar = () => {
     return (
@@ -7,14 +31,29 @@ export const NavBar = () => {
             <div className="flex-none">
                 <a href="/">BayBoyz</a>
             </div>
-            <nav></nav>
-            <nav className="flex-1">
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
+            <nav>
+                <div className="flex gap-1">
+                    {menuLinks.map((menu) => (
+                        <a key={menu.id} href={`/${menu.url}`}>
+                            <h3 className="text-black">{menu.title}</h3>
+                        </a>
+                    ))}
+                </div>
+            </nav>
+            <nav className="flex-1 flex gap-2">
+                <div>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                </div>
+                <div>
+                    <a href="/cart">
+                        <ShoppingBagIcon />
+                    </a>
+                </div>
             </nav>
         </header>
     )
